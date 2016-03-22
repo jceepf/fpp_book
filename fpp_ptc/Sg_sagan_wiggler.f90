169f8cda4c0ddbbc4138a77e93725fabfe23616a
jceepf <jceepf@hotmail.com>
2016-03-22 15:23:53 -0400

    *

--- fpp_ptc/Sg_sagan_wiggler.f90
+++ fpp_ptc/Sg_sagan_wiggler.f90
@@ -1445,7 +1445,7 @@ contains
           A =   -EL%W%A(I)*coseh(EL%W%K(1,i)*(X(1)+EL%W%X0(i)))*sin(EL%W%K(2,i)*(X(3)+EL%W%Y0(I)))* &
                 sin(EL%W%K(3,i)*Z+EL%W%F(I))/EL%W%K(3,i) + A
           B =   -EL%W%A(I)*EL%W%K(1,i)*EL%W%K(2,i)*sineh(EL%W%K(1,i)*(X(1)+EL%W%X0(i)))*Sinx_x(EL%W%K(2,i)*(X(3)+EL%W%Y0(I))*0.5_DP )**2 &
-                *sin(EL%W%K(3,i)*Z+EL%W%F(I))*(EL%W%K(2,i)*(X(3)+EL%W%Y0(I)))**2*0.5_DP/EL%W%K(3,i) + B
+                *sin(EL%W%K(3,i)*Z+EL%W%F(I))*(X(3)+EL%W%Y0(I))**2*0.5_DP/EL%W%K(3,i) + B
        elseif (EL%W%FORM(I) == hyper_y_family_sq) THEN
           A =   EL%W%A(I)*(EL%W%K(1,i)/EL%W%K(3,i))*sin(EL%W%K(1,i)*(X(1)+EL%W%X0(i)))*coseh(EL%W%K(2,i)*(X(3)+EL%W%Y0(I)))* &
                 SIN(EL%W%K(3,i)*Z+EL%W%F(I))/EL%W%K(3,i) + A
@@ -1525,7 +1525,7 @@ ENDDO
           A =   -EL%W%A(I)*cosh(EL%W%K(1,i)*(X(1)+EL%W%X0(i)))*sin(EL%W%K(2,i)*(X(3)+EL%W%Y0(I)))* &
                 sin(EL%W%K(3,i)*Z+EL%W%F(I))/EL%W%K(3,i) + A
           B =   -EL%W%A(I)*EL%W%K(1,i)*EL%W%K(2,i)*sinh(EL%W%K(1,i)*(X(1)+EL%W%X0(i)))*Sinx_x(EL%W%K(2,i)*(X(3)+EL%W%Y0(I))*0.5_DP )**2 &
-                *sin(EL%W%K(3,i)*Z+EL%W%F(I))*(EL%W%K(2,i)*(X(3)+EL%W%Y0(I)))**2*0.5_DP/EL%W%K(3,i) + B
+                *sin(EL%W%K(3,i)*Z+EL%W%F(I))*(X(3)+EL%W%Y0(I))**2*0.5_DP/EL%W%K(3,i) + B
        elseif (EL%W%FORM(I) == hyper_y_family_sq) THEN
           A =   EL%W%A(I)*(EL%W%K(1,i)/EL%W%K(3,i))*sin(EL%W%K(1,i)*(X(1)+EL%W%X0(i)))*cosh(EL%W%K(2,i)*(X(3)+EL%W%Y0(I)))* &
                 SIN(EL%W%K(3,i)*Z+EL%W%F(I))/EL%W%K(3,i) + A
