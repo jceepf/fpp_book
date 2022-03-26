40bd93fd5bb40e10e5c4faf44677d18320b7bacb
jceepf <jceepf@users.noreply.github.com>
2022-03-26 09:27:17 +0900

    c_canonise fixed
    Growth due to damping

--- fpp_ptc/Se_status.f90
+++ fpp_ptc/Se_status.f90
@@ -1508,7 +1508,8 @@ CONTAINS
     LOGICAL(lp), optional, INTENT(IN):: pack
     INTEGER, INTENT(IN):: NO1,NP1
     INTEGER,optional :: ND2,NPARA,number_of_clocks
-
+     do_damping=.false.
+    if(state%radiation) do_damping=.true.
     use_complex_in_ptc=my_true
     call S_init(STATE,NO1,NP1,pack,ND2,NPARA,number_of_clocks)
    end subroutine init_all
@@ -1523,6 +1524,8 @@ CONTAINS
     INTEGER,optional :: ND2,NPARA,number_of_clocks
     INTEGER  ND2l,NPARAl,n_acc,no1c,nv,i
     LOGICAL(lp) package
+     do_damping=.false.
+    if(state%radiation) do_damping=.true.
     n_rf=0
 !    call dd_p !valishev
     doing_ac_modulation_in_ptc=.false.
